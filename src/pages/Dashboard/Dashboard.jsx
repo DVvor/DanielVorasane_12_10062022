e9fa5696fa75196cbf935164d9c3da5390b88719
DVvor <85024600+DVvor@users.noreply.github.com>
2022-07-04 16:07:49 +0200

    change component-error when user datas are not found

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import DailyActivityChart from '../../Components/Charts/DailyActivityChart/Daily
 import AverageSessionTimeChart from '../../Components/Charts/AverageSessionTimeChart/AverageSessionTimeChart'
 import PerformanceRadarChart from '../../Components/Charts/PerformanceChart/PerformanceChart'
 import ScoreChart from '../../Components/Charts/ScoreChart/ScoreChart'
+import ErrorPage from '../ErrorPage/ErrorPage.jsx'
 
 // Data API
 import { getUserInfos, getUserActivities, getUserAverageSessions, getUserPerformance } from "../../Datas/getUserDatas"
@@ -24,23 +25,37 @@ import { getUserInfos, getUserActivities, getUserAverageSessions, getUserPerform
 function Dashboard() {
   const { id } = useParams(); // get current page ID
 
+  const [error, setError] = useState(null);
+  const [dataIsLoaded, setdataIsLoaded] = useState(false);
+
   const [userInfosDatas, setUserInfosDatas] = useState({})
   const [userActivitiesDatas, setUserActivitiesDatas] = useState([]);
   const [userSessionsDatas, setUserSessionsDatas] = useState();
   const [userPerformanceDatas, setUserPerformanceDatas] = useState({});
-    
+
+
   async function fetchData(userId) {
-    const userInfosResult = await getUserInfos(userId)
-    setUserInfosDatas(userInfosResult)
-
-    const userActivitiesResult = await getUserActivities(userId)
-    setUserActivitiesDatas(userActivitiesResult)
-
-    const userSessionsResult = await getUserAverageSessions(userId)
-    setUserSessionsDatas(userSessionsResult)
-
-    const userPerformanceResult = await getUserPerformance(userId)
-    setUserPerformanceDatas(userPerformanceResult)
+    setdataIsLoaded(false)
+    try {
+      const userInfosResult = await getUserInfos(userId)
+      setUserInfosDatas(userInfosResult)
+  
+      const userActivitiesResult = await getUserActivities(userId)
+      setUserActivitiesDatas(userActivitiesResult)
+  
+      const userSessionsResult = await getUserAverageSessions(userId)
+      setUserSessionsDatas(userSessionsResult)
+  
+      const userPerformanceResult = await getUserPerformance(userId)
+      setUserPerformanceDatas(userPerformanceResult)
+    }
+    catch (error) {
+      console.log(error)
+      setError(true)
+    }
+    finally {
+      setdataIsLoaded(true)
+    }
   }
 
   useEffect(() => {
@@ -49,32 +64,42 @@ function Dashboard() {
   
     const username = userInfosDatas.userInfos?.firstName
 
-    return (
-  <>
-    <div className='dashboard'>
-      <div className='dashboard-header'>
-        <h1 className='title'>Bonjour <span className='name'>{username}</span></h1>
-        <p className='message'>Félicitation ! Vous avez explosé vos objectifs hier <span role="img" aria-label="congratulation">👏</span></p>
-      </div>
-      
-      <div className='dashboard-content'>
-        <div className='section-chart' >
-            <DailyActivityChart data={userActivitiesDatas} />
-            <AverageSessionTimeChart data={userSessionsDatas}/>
-            <PerformanceRadarChart datas={userPerformanceDatas} />
-            <ScoreChart data={userInfosDatas}/>
-        </div>
-        <div className='section-card'>
-          <Card type="Calories"  data={userInfosDatas.keyData}/>
-          <Card type="Proteines" data={userInfosDatas.keyData}/>
-          <Card type="Glucides" data={userInfosDatas.keyData}/>
-          <Card type="Lipides" data={userInfosDatas.keyData}/>
-        </div>
-      </div>
-    </div>
-  </>
-  )
 
+  // if data not found, display component error page
+  if (error) {
+    return (
+        <ErrorPage/>
+    )
+  }
+
+  // if datas are found, display components with user datas
+  if (dataIsLoaded) {
+    return (
+    <>
+      <div className='dashboard'>
+        <div className='dashboard-header'>
+          <h1 className='title'>Bonjour <span className='name'>{username}</span></h1>
+          <p className='message'>Félicitation ! Vous avez explosé vos objectifs hier <span role="img" aria-label="congratulation">👏</span></p>
+        </div>
+        
+        <div className='dashboard-content'>
+          <div className='section-chart' >
+              <DailyActivityChart data={userActivitiesDatas} />
+              <AverageSessionTimeChart data={userSessionsDatas}/>
+              <PerformanceRadarChart datas={userPerformanceDatas} />
+              <ScoreChart data={userInfosDatas}/>
+          </div>
+          <div className='section-card'>
+            <Card type="Calories"  data={userInfosDatas.keyData}/>
+            <Card type="Proteines" data={userInfosDatas.keyData}/>
+            <Card type="Glucides" data={userInfosDatas.keyData}/>
+            <Card type="Lipides" data={userInfosDatas.keyData}/>
+          </div>
+        </div>
+      </div>
+    </>
+    )
+  }
 }
 
 
