85652293bd6aea90f8844a5f72e6f81d6044a878
DVvor <85024600+DVvor@users.noreply.github.com>
2022-07-04 16:25:30 +0200

    change fetch getuserdatas

--- src/datas/getUserDatas.js
+++ src/datas/getUserDatas.js
@@ -3,19 +3,35 @@
 // user information (first name, last name and age), 
 // the current day's score (todayScore) and key data (calorie, macronutrient, etc.)
 
+// async function getUserInfos(userId) {
+//   const data = await fetch(`http://localhost:3000/user/${userId}`) 
+//   .then(function (result) {
+//     if (result.ok) {
+//       return result.json()
+//     }
+//   })
+//   .catch(function (error) {
+//     console.log(error)
+//   })
+//   // console.log(data.data)
+//   return (data.data)
+// }
+
 async function getUserInfos(userId) {
-  const data = await fetch(`http://localhost:3000/user/${userId}`) 
-  .then(function (result) {
-    if (result.ok) {
-      return result.json()
+  let data = ""
+  try {
+    let response = await fetch(`http://localhost:3000/user/${userId}`)
+    if (response.ok){
+      data = await response.json()
+      return data.data
     }
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
-  // console.log(data.data)
-  return (data.data)
+  }
+  catch (err) {
+  console.log(err)
+  }
 }
+
+
 
 // Get user Actvities (Kilograms and calories lost per day)
 async function getUserActivities(userId) {
